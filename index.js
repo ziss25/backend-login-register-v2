503be246863e54adfb745c16e4e90d06b4499302
ziss aja <ziss2517@gmail.com>
2023-09-16 16:40:41 +0800

    [other] izinkan credentials cookie

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ try {
   console.error(error);
 }
 
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173', 'https://anime-list-app-five.vercel.app'], credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
