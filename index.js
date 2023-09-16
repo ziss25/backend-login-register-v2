d60519543742eb8991157bb0d6247b450d8dab3d
ziss aja <ziss2517@gmail.com>
2023-09-16 18:40:50 +0800

    capek 2

--- index.js
+++ index.js
@@ -20,13 +20,7 @@ try {
   console.error(error);
 }
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true); // Jika Anda ingin mengizinkan kredensial (cookie)
-  next();
-});
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
