5d4c561952d5a6d360bc9b6311c05970dc06ae7c
ziss aja <ziss2517@gmail.com>
2023-09-16 18:26:54 +0800

    fix capek

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ try {
 }
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true); // Jika Anda ingin mengizinkan kredensial (cookie)
