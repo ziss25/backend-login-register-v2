6dc2ed845dfc2fe4647a9ad8f1ec120cbdc15150
ziss aja <ziss2517@gmail.com>
2023-09-16 18:22:16 +0800

    izinkan semua domain

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ try {
   console.error(error);
 }
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true); // Jika Anda ingin mengizinkan kredensial (cookie)
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
