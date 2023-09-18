f352725e70617d111ef1adc96e3b86e84ceab912
ziss aja <ziss2517@gmail.com>
2023-09-18 14:07:13 +0800

    testing2

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ try {
   console.error(error);
 }
 
+app.set('trust proxy', 1);
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
