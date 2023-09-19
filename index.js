95e0bb973187cc1bac09fc3a9692e5298d5be449
ziss aja <ziss2517@gmail.com>
2023-09-19 12:25:52 +0800

    [optimalize] penambahan env untuk dynamic origin domain

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import cors from 'cors';
 import FileUpload from 'express-fileupload';
 dotenv.config();
 
+const domain = process.env.ORIGIN_DOMAIN == 'production;' ? 'https://anime-list-app-five.vercel.app' : 'http://localhost:5173';
+
 const app = express();
 
 try {
@@ -20,7 +22,8 @@ try {
   console.error(error);
 }
 
-app.use(cors({ origin: 'https://anime-list-app-five.vercel.app', credentials: true }));
+console.log(domain);
+app.use(cors({ origin: domain, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
